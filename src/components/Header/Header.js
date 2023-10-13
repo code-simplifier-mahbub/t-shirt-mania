2d47d161600698bd2cb3949ce42ef11409162a51
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2023-10-13 10:21:44 +0600

    [DONE] create a new router project with context API, devtool

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>This is header section</h2>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Header;
