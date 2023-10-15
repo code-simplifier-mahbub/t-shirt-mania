08fa2dc65276d95b36438eb0d18022756cb6c8c5
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2023-10-15 12:50:06 +0600

    [DONE] conditional rendaring execute in react

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import './cart.css'
+import './Cart.css'
 
 const Cart = ({cart,handleToRemove}) => {
+
+    let message;
+
+    if(cart.length === 0){
+        message = <p>please buy at least one product</p>
+    }
+    else if(cart.length ===1 ){
+        message = <p>please buy more product</p>
+    }
+    else{
+        message = <p>thank you for buy more  product</p>
+    }
+
+    
     
     return (
         <div>
-            <h2>Order summary</h2>
-            <p>Total quantity : {cart.length}</p>
+            <h2 className={cart.length === 2 ? `yellow` : 'purple'}>Order summary</h2>
+            <p className={`bold ${cart.length === 2 ? 'green' : 'orange'}`}>Total quantity : {cart.length}</p>
            {
             cart.map(tshirt => 
             <p
@@ -20,6 +34,18 @@ const Cart = ({cart,handleToRemove}) => {
             
             )
             
+           }
+           {
+            message
+           }
+           {
+            message = cart.length === 5 ? <p>Now you are our member</p> : <p>keep going to get membership</p>
+           }
+           {
+            message = cart.length ===4 && <p> you are getting our membership as soon</p>
+           }
+           {
+            message = cart.length ===5 || <p> you are not a star</p>
            }
         </div>
     );
